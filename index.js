d521adf6112bf424e155e7eb84f54f574596b840
Andres Caceres <acaceres@espol.edu.ec>
2021-09-24 01:00:56 -0500

    [CASE_DIVISIBLE_4] Green impl. Renaming variables, improving code

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const check = function(value) {
-  if(value % 400 == 0) return true;
-  if(value % 100 == 0 && value % 4 == 0) return false;
-  if(value % 4 == 0) return true;
+/* divisible values to be considered inside our function */
+const DIVISIBLE_400 = 400;
+const DIVISIBLE_100 = 100;
+const DIVISIBLE_4 = 4;
 
-   return false;
+/*@param [value] int */
+const check = function(value) {
+  if(value % DIVISIBLE_400 == 0) return true;
+  if(value % DIVISIBLE_100 == 0 && value % DIVISIBLE_4 == 0) return false;
+
+  return (value % DIVISIBLE_4 == 0);
 };
 
 
